3ee98dcd5a8978b443ee83997f7018d86d7d3577
Prasanth P <prasanthai01@brightskills.com>
2024-01-29 10:45:30 +0530

    program finished

--- Cplus/cplusconstructovrload/main.cpp
+++ Cplus/cplusconstructovrload/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class  functionoverloademo
+class constructoversample
 {
 public:
-
-    int string
+    constructoversample()
+    {
+        cout << "New film list \n";
+    }
+    constructoversample(string movie1)
+    {
+        cout << "The first new film watch in sunday is " << movie1 << endl;
+    }
+    constructoversample(string movie1,string movie2)
+    {
+        cout << "\n The Second new film watch in last week are " << movie1 << "," << movie2 << endl;
+    }
+    constructoversample(string movie1, string movie2,string movie3)
+    {
+        cout << "\n The Third new film watch in last last night " << movie1 << "," << movie2 << "," << movie3 << endl;
+    }
 };
+int main()
+{
+
+    string movie1,movie2,movie3;
+    constructoversample cs;
+    cin >> movie1;
+    constructoversample cs1(movie1);
+     cin >> movie1 >> movie2;
+    constructoversample cs2(movie1, movie2);
+     cin >> movie1>> movie2>>movie3;
+    constructoversample cs3(movie1, movie2,movie3);
+
+
+    return 0;
+}
