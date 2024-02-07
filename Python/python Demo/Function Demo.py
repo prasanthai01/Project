9db51d04294adeb31e573bc97fab4d5bbe7e7ead
Prasanthai01 <prasanthai01@brightskills.com>
2024-02-07 13:09:53 +0530

    added files

---
+++ Python/python Demo/Function Demo.py
@@ -0,0 +1,14 @@
+def myfunction():
+    a=1+2
+    print(a)
+    if a==3:
+        print("a is Three")   
+        x=("I can't accept it")
+        for x1 in x:
+          print(x1)
+        for x2 in x.split(" "):  
+          print(x2)
+    y= input("Enter the value :").split()
+    print(y)
+             
+myfunction()
